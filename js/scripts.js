a89541b1138854856fe5fa390c47462d315043c3
Student <example@example.com>
2019-10-28 13:56:02 -0700

    add functioning js

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,19 @@ $(document).ready(function(){
   $(".groceries").submit(function(event){
     event.preventDefault();
     var items = ['item1', 'item2', 'item3', 'item4']
-    items = items.map(function(item){
+    var newItems = items.map(function(item){
 
-      var userInput = $("input#" + item).val();
+      return $("input#" + item).val();
 
 
-      $(".result").append("<li>"+ userInput.toUpperCase() +"</li>")
+
       console.log(item.toUpperCase());
 
     });
+    newItems.sort();
+    newItems.forEach(function(item) {
+      $(".result").append("<li>"+ item.toUpperCase() +"</li>");
+
+    })
     });
   });
